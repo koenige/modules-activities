ea8ce89c5783eba8d152a89cff38acfe77b87160
Gustaf Mossakowski <gustaf@koenige.org>
2020-11-30 10:50:13 +0000

    activities: change database structure, add activity_uri to UNIQUE index
    git-svn-id: file:///Users/Shared/Repos/modules/activities/trunk@18 131b83a9-4073-4f6e-86c2-572d38a4c19f

--- docs/sql/update.sql
+++ docs/sql/update.sql
@@ -10,3 +10,6 @@
  */
 
 /* 2020-09-10-1 */	ALTER TABLE `usergroups` CHANGE `usergroup` `usergroup` varchar(80) COLLATE 'utf8mb4_unicode_ci' NOT NULL AFTER `usergroup_id`;
+/* 2020-11-30-1 */	ALTER TABLE `activities` CHANGE `activity_uri` `activity_uri` varchar(255) COLLATE 'utf8mb4_unicode_ci' NOT NULL AFTER `activity_ip`;
+/* 2020-11-30-2 */	ALTER TABLE `activities` CHANGE `activity_uri` `activity_uri` varchar(255) COLLATE 'latin1_general_ci' NOT NULL AFTER `activity_ip`;
+/* 2020-11-30-3 */	ALTER TABLE `activities` ADD UNIQUE `participation_id_activity_category_id_activity_date_activity_uri` (`participation_id`, `activity_category_id`, `activity_date`, `activity_uri`), DROP INDEX `participation_id_activity_category_id_activity_date`;
