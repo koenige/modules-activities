3f9e3afd063fecc9c25e536100810a431f450bb5
Gustaf Mossakowski <gustaf@koenige.org>
2021-02-17 19:27:32 +0000

    participations/usergroups: read parameters from usergroup_category_id, usergroup_id; hide[]  = field_name hides field name in form; value[field_name] = value sets value for a field and hides it, too
    git-svn-id: file:///Users/Shared/Repos/modules/activities/trunk@31 131b83a9-4073-4f6e-86c2-572d38a4c19f

--- zzbrick_forms/participations-usergroups.php
+++ zzbrick_forms/participations-usergroups.php
@@ -17,6 +17,8 @@ require __DIR__.'/../zzbrick_tables/participations.php';
 if (empty($brick['vars'])) wrap_quit(404);
 
 $sql = 'SELECT usergroup_id, usergroup, identifier, usergroups.description
+		, categories.parameters AS category_parameters
+		, usergroups.parameters AS usergroup_parameters
 	FROM usergroups
 	LEFT JOIN categories
 		ON usergroups.usergroup_category_id = categories.category_id
@@ -25,7 +27,36 @@ $sql = sprintf($sql, wrap_db_escape($brick['vars'][0]));
 $data = wrap_db_fetch($sql);
 if (!$data) wrap_quit(404);
 
+parse_str($data['category_parameters'], $parameters);
+parse_str($data['usergroup_parameters'], $u_parameters);
+$parameters += $u_parameters;
+
 $zz['where']['usergroup_id'] = $data['usergroup_id'];
 $zz['title'] = $data['usergroup'];
 
 $zz['fields'][2]['type'] = 'write_once';
+
+if (!empty($parameters['hide'])) {
+	foreach ($parameters['hide'] as $field_name) {
+		foreach ($zz['fields'] as $no => $field) {
+			if ($field['field_name'] !== $field_name) continue;
+			$zz['fields'][$no]['hide_in_form'] = true;
+			$zz['fields'][$no]['hide_in_list'] = true;
+		}	
+	}
+}
+
+if (!empty($parameters['value'])) {
+	foreach ($parameters['value'] as $field_name => $value) {
+		foreach ($zz['fields'] as $no => $field) {
+			if ($field['field_name'] !== $field_name) continue;
+			$zz['fields'][$no]['type'] = 'hidden';
+			if (wrap_substr($field_name, 'category_id', 'end'))
+				$zz['fields'][$no]['value'] = wrap_category_id($value);
+			else
+				$zz['fields'][$no]['value'] = $value;
+			$zz['fields'][$no]['hide_in_form'] = true;
+			$zz['fields'][$no]['hide_in_list'] = true;
+		}
+	}
+}
