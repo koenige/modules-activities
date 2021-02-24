530325b6f15462011ee430198e008c8ea56110d5
Gustaf Mossakowski <gustaf@koenige.org>
2021-02-24 11:46:14 +0000

    contacts/access: add function mf_activities_contact_access() to check access per data; only arrays with category_id will be checked, [public]_access and individual rights are saved
    git-svn-id: file:///Users/Shared/Repos/modules/activities/trunk@36 131b83a9-4073-4f6e-86c2-572d38a4c19f

--- activities/_functions.inc.php
+++ activities/_functions.inc.php
@@ -52,3 +52,61 @@ function mf_activities_group_path($values) {
 	}
 	return sprintf($zz_setting['activities_profile_path']['usergroup'], $values['identifier']);
 }
+
+/**
+ * check for access rights for contact data, mark content
+ * with publish=1 or publish=0
+ *
+ * @param array $contact
+ * @param string $access
+ */
+function mf_activities_contact_access($contact, $access) {
+	static $access_properties;
+	if (empty($access_properties)) {
+		$sql = 'SELECT categories.category_id, categories.category
+				, categories.parameters, categories.path
+				, main_categories.parameters AS main_parameters
+				, main_categories.path AS main_path
+			FROM categories
+			LEFT JOIN categories main_categories
+				ON categories.main_category_id = main_categories.category_id
+			WHERE main_categories.parameters LIKE "%&access_property=1%"';
+		$access_properties = wrap_db_fetch($sql, 'category_id');
+	}
+	
+	$sql = 'SELECT properties.category_id
+			, access.parameters AS parameters
+		FROM contacts_access
+		LEFT JOIN usergroups USING (usergroup_id)
+		LEFT JOIN categories access
+			ON access.category_id = contacts_access.access_category_id
+		LEFT JOIN categories properties
+			ON properties.category_id = contacts_access.property_category_id
+		WHERE contact_id = %d
+		AND (usergroups.identifier = "%s" OR usergroups.parameters LIKE "%%&alias=%s%%")';
+	$sql = sprintf($sql
+		, $contact['contact_id']
+		, wrap_db_escape($access)
+		, wrap_db_escape($access)
+	);
+	$access_data = wrap_db_fetch($sql, 'category_id');
+	
+	foreach ($contact as $key => $value) {
+		if (!is_array($value)) continue;
+		foreach ($value as $detail_key => $detail_value) {
+			if (!array_key_exists('category_id', $detail_value)) continue;
+			if (!array_key_exists($detail_value['category_id'], $access_properties)) continue;
+			if (!array_key_exists($detail_value['category_id'], $access_data)) {
+				$contact[$key][$detail_key][$access.'_access'] = false;
+			} else {
+				parse_str($access_data[$detail_value['category_id']]['parameters'], $parameters);
+				$contact[$key][$detail_key][$access.'_access'] = true;
+				foreach ($parameters as $pkey => $pvalue) {
+					if ($pkey === 'contacts_access') continue;
+					$contact[$key][$detail_key][$access.'_'.$pkey] = $pvalue;
+				}
+			}
+		}
+	}
+	return $contact;
+}
