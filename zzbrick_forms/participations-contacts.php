804dc822e1595bf54c25f3f660ded6e824f9d2d5
Gustaf Mossakowski <gustaf@koenige.org>
2021-03-02 10:15:31 +0000

    participations/contacts: add form for a single contact‘s participations
    git-svn-id: file:///Users/Shared/Repos/modules/activities/trunk@40 131b83a9-4073-4f6e-86c2-572d38a4c19f

---
+++ zzbrick_forms/participations-contacts.php
@@ -0,0 +1,35 @@
+<?php 
+
+/**
+ * Zugzwang Project
+ * Table with participations of a contact
+ *
+ * http://www.zugzwang.org/modules/activities
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2021 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+require __DIR__.'/../zzbrick_tables/participations.php';
+
+if (empty($brick['vars'])) wrap_quit(404);
+
+$sql = 'SELECT contact_id, contact
+	FROM contacts
+	WHERE identifier = "%s"';
+$sql = sprintf($sql, wrap_db_escape($brick['vars'][0]));
+$data = wrap_db_fetch($sql);
+if (!$data) wrap_quit(404);
+
+$zz['where']['contact_id'] = $data['contact_id'];
+
+$zz['fields'][10]['hide_in_list'] = true;
+$zz['fields'][10]['hide_in_form'] = true;
+
+$zz['filter'][1]['sql'] = wrap_edit_sql(
+	$zz['filter'][1]['sql'], 'WHERE', sprintf('contact_id = %d', $data['contact_id'])
+);
+
+$zz_conf['referer'] = '../';
