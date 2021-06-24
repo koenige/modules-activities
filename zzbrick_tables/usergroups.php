546365da7a43f1e2f9fe3e6399e38cd624a1e131
Gustaf Mossakowski <gustaf@koenige.org>
2021-06-24 17:18:31 +0000

    usergroups: hide parameters field for non-admins; add colums for no. of members an ex-members
    git-svn-id: file:///Users/Shared/Repos/modules/activities/trunk@64 131b83a9-4073-4f6e-86c2-572d38a4c19f

--- zzbrick_tables/usergroups.php
+++ zzbrick_tables/usergroups.php
@@ -56,6 +56,7 @@ $zz['fields'][7]['field_name'] = 'sequence';
 $zz['fields'][7]['type'] = 'sequence';
 $zz['fields'][7]['hide_in_list'] = true;
 
+$zz['fields'][8]['title'] = wrap_text('Active?');
 $zz['fields'][8]['field_name'] = 'active';
 $zz['fields'][8]['type'] = 'select';
 $zz['fields'][8]['enum'] = ['yes', 'no'];
@@ -64,6 +65,27 @@ $zz['fields'][8]['default'] = 'yes';
 $zz['fields'][9]['field_name'] = 'parameters';
 $zz['fields'][9]['type'] = 'parameter';
 $zz['fields'][9]['hide_in_list'] = true;
+if (!wrap_access('activities_usergroups_parameters')) {
+	$zz['fields'][9]['hide_in_form'] = true;
+}
+
+$zz['fields'][10]['title_tab'] = 'M.';
+$zz['fields'][10]['title'] = 'Members';
+$zz['fields'][10]['field_name'] = 'active_users';
+$zz['fields'][10]['type'] = 'display';
+$zz['fields'][10]['hide_in_form'] = true;
+$zz['fields'][10]['hide_in_list_if_empty'] = true;
+$zz['fields'][10]['exclude_from_search'] = true;
+$zz['fields'][10]['class'] = 'number';
+
+$zz['fields'][11]['title_tab'] = 'E. M.';
+$zz['fields'][11]['title'] = 'Ex-members';
+$zz['fields'][11]['field_name'] = 'inactive_users';
+$zz['fields'][11]['type'] = 'display';
+$zz['fields'][11]['hide_in_form'] = true;
+$zz['fields'][11]['hide_in_list_if_empty'] = true;
+$zz['fields'][11]['exclude_from_search'] = true;
+$zz['fields'][11]['class'] = 'number';
 
 $zz['fields'][99]['field_name'] = 'last_update';
 $zz['fields'][99]['type'] = 'timestamp';
@@ -71,6 +93,14 @@ $zz['fields'][99]['hide_in_list'] = true;
 
 $zz['sql'] = 'SELECT /*_PREFIX_*/usergroups.*, category
 		, /*_PREFIX_*/categories.parameters AS category_parameters
+		, (SELECT COUNT(*) FROM /*_PREFIX_*/participations
+			WHERE /*_PREFIX_*/participations.usergroup_id = /*_PREFIX_*/usergroups.usergroup_id
+			AND ISNULL(participations.date_end) OR participations.date_end > CURRENT_DATE()
+		) AS active_users
+		, (SELECT COUNT(*) FROM /*_PREFIX_*/participations
+			WHERE /*_PREFIX_*/participations.usergroup_id = /*_PREFIX_*/usergroups.usergroup_id
+			AND participations.date_end <= CURRENT_DATE()
+		) AS inactive_users
 	FROM /*_PREFIX_*/usergroups
 	LEFT JOIN /*_PREFIX_*/categories
 		ON /*_PREFIX_*/usergroups.usergroup_category_id = /*_PREFIX_*/categories.category_id
