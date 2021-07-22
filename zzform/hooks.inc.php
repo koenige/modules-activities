58b8bb325cff7a61611f6800679d2a5666284eb0
Gustaf Mossakowski <gustaf@koenige.org>
2021-07-22 22:53:47 +0000

    registration: add hook to add activity, send confirmation mail; add mail templates
    git-svn-id: file:///Users/Shared/Repos/modules/activities/trunk@73 131b83a9-4073-4f6e-86c2-572d38a4c19f

--- zzform/hooks.inc.php
+++ zzform/hooks.inc.php
@@ -104,3 +104,83 @@ function mf_activities_copy_translations($table, $ids, $map) {
 	}
 	return true;
 }
+
+/**
+ * send confirmation mail for registration
+ * add activity for registration
+ * 
+ * @param array $ops
+ * @return array
+ */
+function mf_activities_confirm_registration($ops) {
+	global $zz_conf;
+	global $zz_setting;
+
+	// get registrant’s data
+	$data = [];
+	$events = [];
+
+	foreach ($ops['return'] as $index => $table) {
+		switch ($table['table']) {
+			case 'contacts':
+				$data['contact'] = $ops['record_new'][$index]['contact'];
+				$data['contact_id'] = $ops['record_new'][$index]['contact_id'];
+				$zz_conf['user'] = $ops['record_new'][$index]['identifier'];
+				break;
+			case 'contactdetails':
+				if ($ops['record_new'][$index]['provider_category_id'] !== wrap_category_id('provider/e-mail')) break;
+				$data['e_mail'] = $ops['record_new'][$index]['identification'];
+				break;
+			case 'participations':
+				$data['verification_hash'][] = $ops['record_new'][$index]['verification_hash'];
+				$data['participation_id'][] = $ops['record_new'][$index]['participation_id'];
+				$events[] = $ops['record_new'][$index]['event_id'];
+				break;
+		}
+	}
+
+	// write activities
+	foreach ($data['participation_id'] as $participation_id) {
+		$values = [];
+		$values['action'] = 'insert';
+		$values['ids'] = ['activity_category_id'];
+		$values['POST']['participation_id'] = $participation_id;
+		$values['POST']['activity_category_id'] = wrap_category_id('activities/subscribe');
+		$values['POST']['activity_uri'] = sprintf('mailto:%s', $data['e_mail']);
+		$activity = zzform_multi('activities', $values);
+		if (!$activity['id'])
+			wrap_error(sprintf('The registration for %s was not completed.', $data['e_mail']), E_USER_ERROR);
+	}
+
+	// get events
+	if ($events) {
+		$sql = 'SELECT event_id, event
+				, CONCAT(IFNULL(date_begin, ""), "/", IFNULL(date_end, "")) AS duration
+			FROM events
+			WHERE event_id IN (%s)';
+		$sql = sprintf($sql, implode(',', $events));
+		$data['events'] = wrap_db_fetch($sql, 'event_id');
+	}
+
+	$data['verification_hash'] = implode('-', $data['verification_hash']);
+
+	$mail['to']['name'] = $data['contact'];
+	$mail['to']['e_mail'] = $data['e_mail'];
+	
+	$data['sender'] = wrap_get_setting('own_name');
+	if (!$data['sender']) $data['sender'] = wrap_get_setting('project');
+	$mail['headers']['From']['name'] = $data['sender'];
+	$mail['headers']['From']['e_mail'] = ($own_e_mail = wrap_get_setting('own_e_mail'))
+		? $own_e_mail : $zz_conf['error_mail_from'];
+
+	// @todo use custom confirmation mails from forms-table
+	$mail['message'] = wrap_template('registration-confirmation-mail', $data);
+	$success = wrap_mail($mail);
+	if (!$success) {
+		wrap_error(sprintf(
+			'Registration mail could not be sent to %s (ID %d)', $data['e_mail'], $data['contact_id']
+		));
+	}
+
+	return [];
+}
