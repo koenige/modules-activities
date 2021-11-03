265a90c30832551047757c453548b4bc58517b1a
Gustaf Mossakowski <gustaf@koenige.org>
2021-11-03 16:55:48 +0000

    registration varchars, -texts: bugfixes in queries, better list display
    git-svn-id: file:///Users/Shared/Repos/modules/activities/trunk@90 131b83a9-4073-4f6e-86c2-572d38a4c19f

--- zzbrick_tables/registrationvarchars.php
+++ zzbrick_tables/registrationvarchars.php
@@ -26,7 +26,9 @@ $zz['fields'][2]['sql'] = sprintf('SELECT contact_id, contact
 	WHERE contact_category_id = %d
 	ORDER BY contact', wrap_category_id('contact/person'));
 $zz['fields'][2]['display_field'] = 'contact';
-$zz['fields'][2]['hide_in_list'] = true;
+$zz['fields'][2]['list_append_next'] = true;
+$zz['fields'][2]['list_prefix'] = '<strong>';
+$zz['fields'][2]['list_suffix'] = '</strong> – ';
 
 $zz['fields'][3]['field_name'] = 'formfield_id';
 $zz['fields'][3]['type'] = 'select';
@@ -37,8 +39,9 @@ $zz['fields'][3]['sql'] = 'SELECT formfield_id
 	LEFT JOIN events USING (event_id)
 	ORDER BY identifier, formfields.sequence';
 $zz['fields'][3]['display_field'] = 'formfield';
-$zz['fields'][3]['search'] = 'CONCAT(events, " ", sequence)';
-$zz['fields'][3]['hide_in_list'] = true;
+$zz['fields'][3]['search'] = 'CONCAT(event, " ", formfields.sequence)';
+$zz['fields'][3]['list_append_next'] = true;
+$zz['fields'][3]['list_suffix'] = '<br>';
 
 $zz['fields'][4]['title'] = 'Text';
 $zz['fields'][4]['field_name'] = 'registrationvarchar';
@@ -55,11 +58,11 @@ $zz['subselect']['suffix'] = '';
 
 $zz['sql'] = 'SELECT registrationvarchars.*
 		, contact
-		, CONCAT(events, " ", sequence) AS formfield
+		, CONCAT(event, " ", formfields.sequence) AS formfield
 	FROM registrationvarchars
 	LEFT JOIN contacts USING (contact_id)
 	LEFT JOIN formfields USING (formfield_id)
 	LEFT JOIN forms USING (form_id)
 	LEFT JOIN events USING (event_id)
 ';
-$zz['sqlorder'] = ' ORDER BY form, sequence';
+$zz['sqlorder'] = ' ORDER BY events.identifier, formfields.sequence';
