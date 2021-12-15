1d5c5d2eb63e653032343a044138d70455116d8d
Gustaf Mossakowski <gustaf@koenige.org>
2021-12-15 08:23:20 +0000

    formfields: change title of formfield field to 'Title'
    git-svn-id: file:///Users/Shared/Repos/modules/activities/trunk@98 131b83a9-4073-4f6e-86c2-572d38a4c19f

--- zzbrick_tables/formfields.php
+++ zzbrick_tables/formfields.php
@@ -37,6 +37,7 @@ $zz['fields'][7]['field_name'] = 'sequence';
 $zz['fields'][7]['type'] = 'number';
 $zz['fields'][7]['auto_value'] = 'increment';
 
+$zz['fields'][3]['title'] = 'Title';
 $zz['fields'][3]['field_name'] = 'formfield';
 $zz['fields'][3]['explanation'] = 'The title of the form field that appears on the left side (= what to enter here).';
 $zz['fields'][3]['typo_remove_double_spaces'] = true;
