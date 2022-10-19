162a216245f73a0fe3325cd6134fbdb97ee5f6ae
Gustaf Mossakowski <gustaf@koenige.org>
2022-10-19 09:35:42 +0000

    participations/contacts: fix search
    git-svn-id: file:///Users/Shared/Repos/modules/activities/trunk@118 131b83a9-4073-4f6e-86c2-572d38a4c19f

--- zzbrick_tables/participations-contacts.php
+++ zzbrick_tables/participations-contacts.php
@@ -23,20 +23,20 @@ $zz['fields'][1]['type'] = 'id';
 $zz['fields'][2]['field_name'] = 'participation_id';
 $zz['fields'][2]['type'] = 'select';
 $zz['fields'][2]['sql'] = 'SELECT participation_id, contact, usergroup
-	FROM participations
-	LEFT JOIN contacts USING (contact_id)
-	LEFT JOIN usergroups USING (usergroup_id)
-	ORDER BY contacts.identifier, usergroups.identifier';
+	FROM /*_PREFIX_*/participations
+	LEFT JOIN /*_PREFIX_*/contacts USING (contact_id)
+	LEFT JOIN /*_PREFIX_*/usergroups USING (usergroup_id)
+	ORDER BY /*_PREFIX_*/contacts.identifier, /*_PREFIX_*/usergroups.identifier';
 $zz['fields'][2]['display_field'] = 'participation';
 
 $zz['fields'][3]['title'] = 'Organisation';
 $zz['fields'][3]['field_name'] = 'contact_id';
 $zz['fields'][3]['type'] = 'select';
 $zz['fields'][3]['sql'] = 'SELECT contact_id, contact, identifier
-	FROM contacts
+	FROM /*_PREFIX_*/contacts
 	ORDER BY identifier';
 $zz['fields'][3]['display_field'] = 'contact';
-$zz['fields'][3]['search'] = 'contacts.contact';
+$zz['fields'][3]['search'] = '/*_PREFIX_*/contacts.contact';
 
 $zz['subselect']['sql'] = 'SELECT participation_id, contact
 	FROM /*_PREFIX_*/contacts
@@ -53,4 +53,4 @@ $zz['sql'] = 'SELECT /*_PREFIX_*/participations_contacts.*
 	LEFT JOIN /*_PREFIX_*/contacts
 		ON /*_PREFIX_*/participations.contact_id = /*_PREFIX_*/contacts.contact_id
 ';
-$zz['sqlorder'] = ' ORDER BY organisations.identifier, usergroups.identifier, contacts.identifier';
+$zz['sqlorder'] = ' ORDER BY organisations.identifier, /*_PREFIX_*/usergroups.identifier, /*_PREFIX_*/contacts.identifier';
