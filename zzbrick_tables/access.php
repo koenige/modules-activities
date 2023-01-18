dea93f0995e422d3a66056ab207f39e662143860
Gustaf Mossakowski <gustaf@koenige.org>
2023-01-18 12:54:25 +0000

    access: sort module filter
    git-svn-id: file:///Users/Shared/Repos/modules/activities/trunk@126 131b83a9-4073-4f6e-86c2-572d38a4c19f

--- zzbrick_tables/access.php
+++ zzbrick_tables/access.php
@@ -8,7 +8,7 @@
  * https://www.zugzwang.org/modules/activities
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2021-2022 Gustaf Mossakowski
+ * @copyright Copyright © 2021-2023 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -57,6 +57,7 @@ $zz['filter'][1]['type'] = 'list';
 $zz['filter'][1]['where'] = 'module';
 $zz['filter'][1]['field_name'] = 'module';
 $zz['filter'][1]['sql'] = 'SELECT DISTINCT module, module
-	FROM /*_PREFIX_*/access';
+	FROM /*_PREFIX_*/access
+	ORDER BY module';
 
 $zz_conf['copy'] = true;
