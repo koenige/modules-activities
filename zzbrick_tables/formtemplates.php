92e3eb4838f44e508070b6ab57c1a215f0e342bc
Gustaf Mossakowski <gustaf@koenige.org>
2023-02-16 15:12:30 +0000

    formtemplates: change order of fields; restrict field `formfield_id` to `formfield_id=1` in template_category; activate `unique_ignore_null` to allow only one template per category with empty formfield_id
    git-svn-id: file:///Users/Shared/Repos/modules/activities/trunk@140 131b83a9-4073-4f6e-86c2-572d38a4c19f

--- zzbrick_tables/formtemplates.php
+++ zzbrick_tables/formtemplates.php
@@ -31,15 +31,11 @@ $zz['fields'][2]['if']['where']['hide_in_list'] = true;
 $zz['fields'][2]['if']['where']['hide_in_form'] = true;
 $zz['fields'][2]['display_field'] = 'event';
 
-$zz['fields'][3]['field_name'] = 'template';
-$zz['fields'][3]['explanation'] = 'Content of the template.';
-$zz['fields'][3]['type'] = 'memo';
-$zz['fields'][3]['rows'] = 6;
-
 $zz['fields'][4]['title'] = 'Type';
 $zz['fields'][4]['field_name'] = 'template_category_id';
 $zz['fields'][4]['type'] = 'select';
 $zz['fields'][4]['sql'] = sprintf('SELECT category_id, category
+		, IF(parameters LIKE "%%&formfield=1%%", 1, NULL) AS show_formfield
 	FROM /*_PREFIX_*/categories
 	WHERE main_category_id = %d
 	ORDER BY sequence',
@@ -47,11 +43,19 @@ $zz['fields'][4]['sql'] = sprintf('SELECT category_id, category
 );
 $zz['fields'][4]['display_field'] = 'category';
 $zz['fields'][4]['sql_translate'] = ['category_id' => 'categories'];
+$zz['fields'][4]['dependent_fields'][5]['if_selected'] = 'show_formfield';
+$zz['fields'][4]['sql_ignore'] = ['show_formfield'];
+
+$zz['fields'][3]['field_name'] = 'template';
+$zz['fields'][3]['explanation'] = wrap_text('Content of the template.');
+$zz['fields'][3]['type'] = 'memo';
+$zz['fields'][3]['rows'] = 6;
 
 $zz['fields'][5]['field_name'] = 'formfield_id';
 $zz['fields'][5]['type'] = 'select';
 $zz['fields'][5]['sql'] = 'SELECT formfield_id, formfield FROM formfields ORDER BY sequence';
 $zz['fields'][5]['display_field'] = 'formfield';
+$zz['fields'][5]['hide_in_list_if_empty'] = true;
 
 $zz['fields'][99]['field_name'] = 'last_update';
 $zz['fields'][99]['type'] = 'timestamp';
@@ -75,3 +79,5 @@ $zz['subtitle']['form_id']['sql'] = $zz['fields'][2]['sql'];
 $zz['subtitle']['form_id']['var'] = ['event', 'duration'];
 $zz['subtitle']['form_id']['concat'] = ', ';
 $zz['subtitle']['form_id']['format'][1] = 'wrap_date';
+
+$zz['unique_ignore_null'] = true;
