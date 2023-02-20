67403cfc6951891bfeb5e87b210d8df09ab7f6b2
Gustaf Mossakowski <gustaf@koenige.org>
2023-02-20 12:50:19 +0000

    eventregistration: add form to edit single form
    git-svn-id: file:///Users/Shared/Repos/modules/activities/trunk@149 131b83a9-4073-4f6e-86c2-572d38a4c19f

---
+++ zzbrick_forms/eventregistration.php
@@ -0,0 +1,22 @@
+<?php 
+
+/**
+ * activities module
+ * form script: registrations, based on events
+ *
+ * Part of »Zugzwang Project«
+ * https://www.zugzwang.org/modules/activities
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2023 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+require_once __DIR__.'/eventregistrations.php';
+
+$zz['title'] = 'Registration';
+$zz['where']['event_id'] = $brick['data']['event_id'];
+$zz['access'] = 'edit_only';
+
+$zz_conf['referer'] = '../';
