0e9d0146ae47a319b9260c0449722f7f9cb431c6
Gustaf Mossakowski <gustaf@koenige.org>
2023-03-03 17:48:26 +0000

    usergroups: change condition to include groups from different organisations which have organisation as category to be non-editable
    git-svn-id: file:///Users/Shared/Repos/modules/activities/trunk@168 131b83a9-4073-4f6e-86c2-572d38a4c19f

--- zzbrick_forms/usergroups.php
+++ zzbrick_forms/usergroups.php
@@ -33,11 +33,11 @@ if (wrap_category_id('relations/organisation')) {
 	);
 	$zz['sql'] = wrap_edit_sql($zz['sql'], 'WHERE', sprintf(
 		'(/*_PREFIX_*/usergroups.organisation_contact_id = %d OR NOT ISNULL(/*_PREFIX_*/usergroups_categories.uc_id))'
-		, $brick['data']['contact_id'], 
+		, $brick['data']['contact_id']
 	));
 	$zz['filter'][1]['sql'] = wrap_edit_sql($zz['filter'][1]['sql'], 'WHERE', sprintf(
 		'(/*_PREFIX_*/usergroups.organisation_contact_id = %d OR NOT ISNULL(/*_PREFIX_*/usergroups_categories.uc_id))'
-		, $brick['data']['contact_id'], 
+		, $brick['data']['contact_id']
 	));
 } else {
 	$zz['where']['organisation_contact_id'] = $brick['data']['contact_id'];
@@ -57,7 +57,7 @@ $zz['fields'][12]['type_detail'] = 'hidden';
 $zz['fields'][12]['default'] = $brick['data']['contact_id'];
 
 $zz['conditions'][10]['scope'] = 'record';
-$zz['conditions'][10]['where'] = 'ISNULL(organisation_contact_id)';
+$zz['conditions'][10]['where'] = sprintf('(ISNULL(organisation_contact_id) OR organisation_contact_id != %d)', $brick['data']['contact_id']);
 
 $zz_conf['if'][10]['edit'] = false;
 $zz_conf['if'][10]['delete'] = false;
