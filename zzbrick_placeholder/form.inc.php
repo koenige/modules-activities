86ef0853ae01ee092fb4e8b2f878d47f784f4786
Gustaf Mossakowski <gustaf@koenige.org>
2023-05-01 09:41:17 +0000

    form: check for authentication and confirmation mail templates separately, check for parameter `no_confirmation_mail=1`, `no_authentication_mail=1` in form category
    git-svn-id: file:///Users/Shared/Repos/modules/activities/trunk@184 131b83a9-4073-4f6e-86c2-572d38a4c19f

--- zzbrick_placeholder/form.inc.php
+++ zzbrick_placeholder/form.inc.php
@@ -18,11 +18,18 @@ function mod_activities_placeholder_form($brick) {
 	if (empty($brick['placeholder'])) return $brick;
 	if (empty($brick['data'])) $brick['data'] = [];
 	
-	$sql = 'SELECT event_id, event, identifier, form_id, abstract, description
+	$sql = 'SELECT event_id, event, identifier, form_id, abstract, events.description
 			, IF(published = "yes", 1, NULL) AS published
 			, IF((SELECT COUNT(*) FROM formtemplates
-				WHERE formtemplates.form_id = forms.form_id)
-			>= 2, 1, NULL) AS formtemplates
+				WHERE formtemplates.form_id = forms.form_id
+				AND template_category_id = %d)
+			>= 1, NULL, IF(form_categories.parameters LIKE "%%&no_authentication_mail=1%%", NULL, 1
+			)) AS formtemplates_authentication_missing
+			, IF((SELECT COUNT(*) FROM formtemplates
+				WHERE formtemplates.form_id = forms.form_id
+				AND template_category_id = %d)
+			>= 1, NULL, IF(form_categories.parameters LIKE "%%&no_confirmation_mail=1%%", NULL, 1
+			)) AS formtemplates_confirmation_missing
 			, IF((SELECT COUNT(*) FROM formfields
 				WHERE formfields.form_id = forms.form_id)
 			>= 2, 1, NULL) AS formfields
@@ -30,16 +37,22 @@ function mod_activities_placeholder_form($brick) {
 		FROM events
 		LEFT JOIN forms USING (event_id)
 		LEFT JOIN websites USING (website_id)
+		LEFT JOIN categories form_categories
+			ON forms.form_category_id = form_categories.category_id
 		WHERE identifier = "%s"
 	    AND event_category_id = %d
 		AND website_id = %d';
 	$sql = sprintf($sql
+		, wrap_category_id('template-types/authentication')
+		, wrap_category_id('template-types/confirmation')
 		, wrap_db_escape($brick['placeholder'])
 		, wrap_category_id('event/registration')
 		, !empty($brick['data']['website_id']) ? $brick['data']['website_id'] : wrap_setting('website_id')
 	);
 	$event = wrap_db_fetch($sql);
 	if (!$event) wrap_quit(404);
+	if (!$event['formtemplates_confirmation_missing'] AND !$event['formtemplates_authentication_missing'])
+		$event['formtemplates'] = true;
 	$brick['data'] = array_merge($brick['data'], $event);
 
 	// access
