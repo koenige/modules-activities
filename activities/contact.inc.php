6e9b6fc946c9a72229e8a9f3d585c4d92309c065
Gustaf Mossakowski <gustaf@koenige.org>
2024-03-01 15:12:42 +0000

    contact: update function for inclusion in contact request, format has changed
    git-svn-id: file:///Users/Shared/Repos/modules/activities/trunk@267 131b83a9-4073-4f6e-86c2-572d38a4c19f

--- activities/contact.inc.php
+++ activities/contact.inc.php
@@ -8,31 +8,36 @@
  * https://www.zugzwang.org/modules/activities
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2023 Gustaf Mossakowski
+ * @copyright Copyright © 2023-2024 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
 
-function mf_activities_contact($data) {
-	$sql = 'SELECT participation_id
+function mf_activities_contact($data, $ids) {
+	$sql = 'SELECT participation_id, contact_id
 			, usergroup_id, usergroup, identifier
 			, date_begin, date_end, remarks, role
 		FROM participations
 		LEFT JOIN usergroups USING (usergroup_id)
 		LEFT JOIN categories
 			ON participations.status_category_id = categories.category_id
-		WHERE contact_id = %d';
-	$sql = sprintf($sql, $data['contact_id']);
-	$data['participations'] = wrap_db_fetch($sql, 'participation_id');
-	foreach ($data['participations'] as $participation_id => $participation) {
-		$data['participations'][$participation_id]['profile_path']
+		WHERE contact_id IN (%s)';
+	$sql = sprintf($sql, implode(',', $ids));
+	$participations = wrap_db_fetch($sql, 'participation_id');
+	// @todo translations
+	
+	foreach ($participations as $participation_id => $participation) {
+		if (empty($data[$participation['contact_id']]['participations'])) {
+			$data[$participation['contact_id']]['participation_contact_path']
+				= mf_activities_contact_path([
+					'identifier' => $data[$participation['contact_id']]['identifier']
+					, 'category_parameters' => 'type='.$data[$participation['contact_id']]['scope']
+				]);
+		}
+		$participation['profile_path']
 			= mf_activities_group_path(['identifier' => $participation['identifier']]);
+		$data[$participation['contact_id']]['participations'][$participation['participation_id']] = $participation;
 	}
-	$data['participation_contact_path']
-		= mf_activities_contact_path([
-			'identifier' => $data['identifier']
-			, 'category_parameters' => 'type='.$data['scope']
-		]);
 	$data['templates']['contact_6'][] = 'contact-participations';
 	return $data;
 }
