73b9d757606ab736d65f6116a5014f5355b18b4b
Gustaf Mossakowski <gustaf@koenige.org>
2024-04-11 07:42:09 +0000

    system.sql: add usergroup IDs
    git-svn-id: file:///Users/Shared/Repos/modules/activities/trunk@287 131b83a9-4073-4f6e-86c2-572d38a4c19f

---
+++ configuration/system.sql
@@ -0,0 +1,15 @@
+/**
+ * activities module
+ * SQL queries for core, page, auth and database IDs
+ *
+ * Part of »Zugzwang Project«
+ * https://www.zugzwang.org/modules/activities
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2024 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+-- ids_usergroups --
+SELECT identifier, usergroup_id FROM usergroups;
