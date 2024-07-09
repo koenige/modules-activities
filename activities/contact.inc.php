f3177068b51ef5771e8fb3762a12f2897ef3cb8e
Gustaf Mossakowski <gustaf@koenige.org>
2024-07-09 14:06:38 +0000

    contacts/access: add form to edit access to contact details, show access to contact details in contact profile, add settings `activities_access_property_prefix`, `activities_contacts_access_hidden`
    git-svn-id: file:///Users/Shared/Repos/modules/activities/trunk@320 131b83a9-4073-4f6e-86c2-572d38a4c19f

--- activities/contact.inc.php
+++ activities/contact.inc.php
@@ -14,6 +14,21 @@
 
 
 function mf_activities_contact($data, $ids) {
+	$data = mf_activities_contact_participations($data, $ids);
+	$data = mf_activities_contact_access_rights($data, $ids);
+	
+	$data['templates']['contact_6'][] = 'contact-participations';
+	return $data;
+}
+
+/**
+ * get participations per contact
+ *
+ * @param array $data existing data
+ * @param array $ids contact IDs
+ * @return array
+ */
+function mf_activities_contact_participations($data, $ids) {
 	wrap_include('functions', 'activities');
 	$sql = 'SELECT participation_id, contact_id
 			, usergroup_id, usergroup, identifier
@@ -39,6 +54,49 @@ function mf_activities_contact($data, $ids) {
 			= mf_activities_group_path(['identifier' => $participation['identifier']]);
 		$data[$participation['contact_id']]['participations'][$participation['participation_id']] = $participation;
 	}
-	$data['templates']['contact_6'][] = 'contact-participations';
+	return $data;
+}
+
+/**
+ * get access rights per contact
+ *
+ * @param array $data existing data
+ * @param array $ids contact IDs
+ * @return array
+ */
+function mf_activities_contact_access_rights($data, $ids) {
+	// is it possible to define access rights?
+	$first = reset($data);
+	
+	$path = wrap_path('activities_contactdata_access['.$first['scope'].']', [], true, true);
+	if (!$path) return $data;
+
+	$sql = 'SELECT contact_access_id, contact_id, usergroup
+			, access.category AS access_category
+			, properties.category AS property_category
+			, areas.category AS area_category
+			, areas.parameters
+		FROM contacts_access
+		LEFT JOIN usergroups USING (usergroup_id)
+		LEFT JOIN categories access
+			ON contacts_access.access_category_id = access.category_id
+		LEFT JOIN categories properties
+			ON contacts_access.property_category_id = properties.category_id
+		LEFT JOIN categories areas
+			ON areas.category_id = properties.main_category_id
+	    WHERE contact_id IN (%s)';
+	$sql = sprintf($sql, implode(',', $ids));
+	$access = wrap_db_fetch($sql, 'contact_access_id');
+	
+	foreach ($access as $contact_access_id => $right) {
+		if ($right['parameters']) parse_str($right['parameters'], $right['parameters']);
+		if (array_key_exists('activities_access_property_prefix', $right['parameters'])) {
+			if ($right['parameters']['activities_access_property_prefix'])
+				$right['area_category'] = $right['parameters']['activities_access_property_prefix'];
+			else
+				$right['area_category'] = '';
+		}
+		$data[$right['contact_id']]['access'][$contact_access_id] = $right;
+	}
 	return $data;
 }
---
+++ zzbrick_forms/contacts-access.php
@@ -0,0 +1,90 @@
+<?php 
+
+/**
+ * activities module
+ * form script: access rights per contact
+ *
+ * Part of »Zugzwang Project«
+ * https://www.zugzwang.org/modules/activities
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2024 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+if (empty($brick['data']['contact_id'])) wrap_quit(404);
+
+//$zz = zzform_include('contacts-access');
+//$zz['where']['contact_id'] = $brick['data']['contact_id'];
+
+$sql = 'SELECT category_id FROM categories
+	WHERE parameters LIKE "%&contacts_access=1%"';
+$categories = wrap_db_fetch($sql, 'category_id');
+
+$zz = zzform_include('usergroups');
+
+$zz['sql'] = wrap_edit_sql($zz['sql'], 'WHERE',
+	sprintf('usergroup_category_id IN (%s)', implode(',', array_keys($categories)))
+);
+
+$zz['sql'] = wrap_edit_sql($zz['sql'], 'WHERE',
+	'active = "yes"'
+);
+$zz['sql'] = wrap_edit_sql($zz['sql'], 'WHERE',
+	'(ISNULL(usergroups.parameters) OR usergroups.parameters NOT LIKE "%&activities_contacts_access_hidden=1%")'
+);
+
+unset($zz['filter']);
+unset($zz['list']['group']);
+
+$zz['access'] = 'edit_details_only';
+
+foreach ($zz['fields'] as $no => $field) {
+	$field_name = $field['field_name'] ?? '';
+	switch ($field_name) {
+	case 'last_update':
+	case 'parameters':
+	case 'active':
+	case 'sequence':
+	case 'usergroup_category_id':
+	case 'identifier':
+		$zz['fields'][$no]['hide_in_form'] = true;
+		$zz['fields'][$no]['hide_in_list'] = true;
+		break;
+	}
+}
+
+$zz['fields'][90] = zzform_include('contacts-access');
+$zz['fields'][90]['title'] = 'Access Rights';
+$zz['fields'][90]['type'] = 'subtable';
+$zz['fields'][90]['sql'] = wrap_edit_sql($zz['fields'][90]['sql'],
+	'WHERE', sprintf('contact_id = %d', $brick['data']['contact_id'])
+);
+$zz['fields'][90]['form_display'] = 'lines';
+$zz['fields'][90]['fields'][2]['type'] = 'hidden';
+$zz['fields'][90]['fields'][2]['hide_in_form'] = true;
+$zz['fields'][90]['fields'][2]['value'] = $brick['data']['contact_id'];
+$zz['fields'][90]['fields'][2]['for_action_ignore'] = true;
+$zz['fields'][90]['fields'][3]['type'] = 'foreign_key';
+$zz['fields'][90]['fields'][5]['for_action_ignore'] = true;
+
+$zz['fields'][90]['subselect']['sql'] = sprintf('SELECT usergroup_id
+		, properties.category AS property_category
+		, access.category AS access_category
+	FROM contacts_access
+	LEFT JOIN categories access
+		ON access.category_id = contacts_access.access_category_id
+	LEFT JOIN categories properties
+		ON properties.category_id = contacts_access.property_category_id
+	WHERE contact_id = %d', $brick['data']['contact_id']);
+$zz['fields'][90]['subselect']['field_prefix'][0] = '<em>';
+$zz['fields'][90]['subselect']['field_suffix'][0] = '</em>: ';
+$zz['fields'][90]['subselect']['concat_rows'] = '<br>';
+
+$zz['title'] = 'Access to Contact Details';
+
+$zz['subtitle']['text'] = $brick['data']['contact'];
+
+$zz['page']['referer'] = '../';
+
